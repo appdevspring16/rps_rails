0e1aa93c2a2d74fade38446145111c6ba98963e9
Leland Brewster <jlbrew3@users.noreply.github.com>
2016-04-18 10:44:06 -0700

    seems to work

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,51 +2,33 @@ class GameController < ApplicationController
   def rps
     @user_move = params[:the_move]
 
-    # ===============================================================
-    # Your code goes below.
-    # The move the user chose is in the variable @user_move.
-    # ===============================================================
-
     u_move = @user_move
+
     moves = ["rock", "paper", "scissors"]
+    comp_move = moves.sample
 
-      if u_move=="rock" || u_move=="paper" || u_move=="scissors"
+    if u_move==comp_move
+      res = "tied"
 
-        comp_move = moves.sample
+    elsif u_move=="rock" && comp_move=="paper"
+      res = "lost"
 
-        if u_move==comp_move
-          res = "It's a tie!"
+    elsif u_move=="rock" && comp_move=="scissors"
+      res = "won"
 
-        elsif u_move=="rock" && comp_move=="paper"
-          res =  "You lose!"
+    elsif u_move=="paper" && comp_move=="rock"
+      res = "won"
 
-        elsif u_move=="rock" && comp_move=="scissors"
-          res =  "You win!"
+    elsif u_move=="paper" && comp_move=="scissors"
+      res = "lost"
 
-        elsif u_move=="paper" && comp_move=="rock"
-          res =  "You win!"
+    elsif u_move=="scissors" && comp_move=="rock"
+      res = "lost"
 
-        elsif u_move=="paper" && comp_move=="scissors"
-          res =  "You lose!"
+    elsif u_move=="scissors" && comp_move=="paper"
+      res = "won"
+    end
 
-        elsif u_move=="scissors" && comp_move=="rock"
-          res =  "You lose!"
-
-        elsif u_move=="scissors" && comp_move=="paper"
-          res =  "You win!"
-        end
-
-      else
-        res = "That doesn't seem to be a valid move."
-
-      end
-
-
-
-    # Your logic here
-
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
 
     @computer_move = comp_move
 
