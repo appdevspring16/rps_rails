7d4135f4d0b49bd51d91447d396f8ac24a22f034
hkerstens <h.kerstens@chicagobooth.edu>
2016-04-17 22:44:24 -0500

    Final
    Deleted all code from my rps game that wasn't relevant, replaced
    user_move with @user_move and computer_move with @computer_move and
    added @result where needed.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,13 +8,34 @@ class GameController < ApplicationController
     # ===============================================================
 
     # Your logic here
+    
+    options = ["rock", "paper", "scissors"]
+    @computer_move = options[rand(3)]
+
+    if @computer_move == @user_move
+      @result = "tie"
+    elsif @computer_move == "rock" && @user_move == "paper"
+      @result = "win"
+    elsif @computer_move == "rock" && @user_move == "scissors"
+      @result = "lose"
+    elsif @computer_move == "paper" && @user_move == "rock"
+      @result = "lose"
+    elsif @computer_move == "paper" && @user_move == "scissors"
+      @result = "win"
+    elsif @computer_move == "scissors" && @user_move == "rock"
+      @result = "win"
+    elsif @computer_move == "scissors" && @user_move == "paper"
+      @result = "lose"
+    end
+
 
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    # @computer_move = "Replace this string with the correct value."
 
-    @result = "Replace this string with the correct value."
+    # @result = "Replace this string with the correct value."
+
 
     # ===============================================================
     # Your code goes above.
