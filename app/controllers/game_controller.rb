d7921b77273bfd8255776bf3c6de80e964733978
meghankinney <meghankinney@users.noreply.github.com>
2016-04-19 02:07:41 +0000

    tried to make it work

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,13 +6,34 @@ class GameController < ApplicationController
     # Your code goes below.
     # The move the user chose is in the variable @user_move.
     # ===============================================================
-#    Meghan Making small change
+    @computer_move = ['rock','paper','scissors'].sample
+
+    if user_move == computer_move
+      puts "tie!"
+    elsif user_move == "paper" && computer_move == "rock"
+      puts "you win!"
+    elsif user_move == "paper" && computer_move == "scissors"
+      puts "you lose!"
+    elsif user_move == "scissors" && computer_move == "rock"
+      puts "you lose!"
+    elsif user_move == "scissors" && computer_move == "paper"
+      puts "you win!"
+    elsif user_move == "rock" && computer_move == "paper"
+      puts "you lose!"
+    elsif user_move == "rock" && computer_move == "scissors"
+      puts "you win!"
+    end
+
+
+
+
+
     # Your logic here
 
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = "The computer chose"
 
     @result = "Replace this string with the correct value."
 
