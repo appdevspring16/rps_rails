2ac4ad87ccea793749f91277a0c26950b4a0edfc
meghankinney <meghankinney@users.noreply.github.com>
2016-04-19 03:15:06 +0000

    end Monday night

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,34 +8,24 @@ class GameController < ApplicationController
     # ===============================================================
     @computer_move = ['rock','paper','scissors'].sample
 
-    if user_move == computer_move
-      puts "tie!"
-    elsif user_move == "paper" && computer_move == "rock"
-      puts "you win!"
-    elsif user_move == "paper" && computer_move == "scissors"
-      puts "you lose!"
-    elsif user_move == "scissors" && computer_move == "rock"
-      puts "you lose!"
-    elsif user_move == "scissors" && computer_move == "paper"
-      puts "you win!"
-    elsif user_move == "rock" && computer_move == "paper"
-      puts "you lose!"
-    elsif user_move == "rock" && computer_move == "scissors"
-      puts "you win!"
+    if @user_move == @computer_move
+      @result = "tie"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @result = "win"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @result = "lose"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @result = "lose"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @result = "win"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @result = "lose"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @result = "win"
     end
 
 
 
-
-
-    # Your logic here
-
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
-
-    @computer_move = "The computer chose"
-
-    @result = "Replace this string with the correct value."
 
     # ===============================================================
     # Your code goes above.
