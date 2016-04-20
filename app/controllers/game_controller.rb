d84babb63686b36377244a2c992afabcb613adab
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-04-20 16:32:38 -0500

    Stopping point 1
    User and computer moves working, but results not working

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,9 +12,28 @@ class GameController < ApplicationController
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = ["Rock", "Paper", "Scissors"].sample
 
-    @result = "Replace this string with the correct value."
+    if @user_move == @computer_move
+      @result= "tied"
+    elsif @user_move == "Rock" && @computer_move == "Paper"
+      @result= "lost"
+    elsif @user_move == "Rock" && @computer_move == "Scissors"
+      @result= "won"
+    elsif @user_move == "Paper" && @computer_move == "Rock"
+      @result= "won"
+    elsif @user_move == "Paper" && @computer_move == "Scissors"
+      @result= "lost"
+    elsif @user_move == "Scissors" && @computer_move == "Rock"
+      @result= "lost"
+    elsif @user_move == "Scissors" && @computer_move == "Paper"
+      @result= "won"
+
+
+
+    @result= ["won"]
+
+    end
 
     # ===============================================================
     # Your code goes above.
