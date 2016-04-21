77641042ababb6d597f71cd8608179544782d2d2
Matthew Sidley <msidley@chicagobooth.edu>
2016-04-21 16:13:37 -0500

    Final

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,14 +7,32 @@ class GameController < ApplicationController
     # The move the user chose is in the variable @user_move.
     # ===============================================================
 
-    # Your logic here
-
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
-
-    @computer_move = "Replace this string with the correct value."
-
-    @result = "Replace this string with the correct value."
+    def comp_move moves
+      return moves.sample
+    end
+    def outcome comp,user_move
+      if comp == user_move
+        return "It's a tie!"
+      elsif comp == "rock" && user_move == "paper"
+        return "You Win"
+      elsif comp == "paper" && user_move == "scissors"
+        return "You Win"
+      elsif comp == "scissors" && user_move == "rock"
+        return "You Win"
+      else
+        return "You Lose"
+      end
+    end
+    def play moves
+      puts "Your move: rock, paper, or scissors?"
+      user_move = @user_move
+      comp = comp_move(moves)
+      result = outcome(comp,user_move)
+      @computer_move = comp
+      @result = result
+    end
+    moves = ["rock","paper","scissors"]
+    play(moves)
 
     # ===============================================================
     # Your code goes above.
