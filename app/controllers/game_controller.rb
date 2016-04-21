c37891896e034843a0b16f9f3eb2cb84305d5ad8
shao313 <shaozhu@chicagobooth.edu>
2016-04-21 09:44:53 -0500

    done

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -11,15 +11,36 @@ class GameController < ApplicationController
 
     # In the end, make sure you assign the correct values to the
     #   following two variables:
+    @computer_move=["rock","paper","scissors"][rand(2)]
 
-    @computer_move = "Replace this string with the correct value."
+    if @user_move == @computer_move
+      @result = "draw"
+    elsif  @user_move=="rock"
+      if @computer_move=="paper"
+        @result = "lose"
+      else
+        @result = "win"
+      end
+    elsif @user_move=="paper"
+      if @computer_move=="scissors"
+        @result = "lose"
+      else
+        @result = "win"
+      end
+    elsif @user_move=="scissors"
+      if @computer_move=="rock"
+        @result = "lose"
+      else
+        @result = "win"
+      end
 
-    @result = "Replace this string with the correct value."
+    end
 
-    # ===============================================================
-    # Your code goes above.
-    # ===============================================================
+  
+  # ===============================================================
+  # Your code goes above.
+  # ===============================================================
 
-    render("rps.html.erb")
-  end
+  render("rps.html.erb")
+end
 end
