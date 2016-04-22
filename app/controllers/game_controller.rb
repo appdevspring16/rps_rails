69556b3d6b3f7ef76c29fee4d84822bb5f072b88
mmuntazir <mehdi.muntazir@outlook.com>
2016-04-21 21:28:47 -0500

    Version 1

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,9 +12,42 @@ class GameController < ApplicationController
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = ["rock", "paper", "scissors"m "quit"].sample
 
     @result = "Replace this string with the correct value."
+
+    puts "Welcome to Rock, Paper, Scissors!"
+
+    puts "rock, paper, pr scissors?"
+
+    user_move = gets.chomp
+
+    puts "You chose #{user_move}"
+
+    choices = ["rock", "paper", "scissors"m "quit"]
+    if user_move="quit"
+      break
+    end
+
+    comp = choices[rand(0..2)]
+
+    new = user_move+comp
+    if new == "rockscissors"
+      puts "You win"
+      elseif new == "rockpaper"
+      puts"You lose"
+      elseif new == "paperscissors"
+      puts "You lose"
+      elseif new == "paperrock"
+      puts "You win"
+      elseif new == "scissorsrock"
+      puts "You lose"
+      elseif new == "scissorspaper"
+      puts "You lose"
+    else
+      puts "Tied"
+    end
+    puts "Computer chose #{new}"
 
     # ===============================================================
     # Your code goes above.
