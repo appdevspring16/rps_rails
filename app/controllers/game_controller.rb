f81459c5d00cfae691bf1f9c0829335be3645412
Zachary C. Cornett <zcornett@chicagobooth.edu>
2016-04-22 17:51:50 -0500

    fail1
    I think I broke it.  I tried to boil down to the simplest code and it
    doesn't work.  :(

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -9,12 +9,39 @@ class GameController < ApplicationController
 
     # Your logic here
 
+    # pc_options = ["rock", "paper","scissors"]
+    # @computer_move=pc_options.sample
+    @computer_move=["rock","paper","scissors"].sample
+
+    if
+      # @computer_move == @user_move
+      # @result="Its a tie!"
+      
+      # elsif
+      @computer_move == "rock" && @user_move == "scissors"
+      @result= "I win"
+      # elsif @computer_move == "scissors" && @user_move =="paper"
+      #   @result= "I win"
+      # elsif @computer_move == "paper" && @user_move =="rock"
+      #   @result= "I win"
+      #
+      # elsif @computer_move =="rock" && @user_move =="paper"
+      #   @result= "You win"
+      # elsif @computer_move="scissors" && @user_move=="rock"
+      #   @result= "You win"
+      # elsif @computer_move=="paper" && @user_move=="scissors"
+      #   @result= "You win"
+    else
+      @result="test"
+    end
+
+
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    # @computer_move = "Replace this string with the correct value."
 
-    @result = "Replace this string with the correct value."
+    # @result = "Replace this string with the correct value."
 
     # ===============================================================
     # Your code goes above.
