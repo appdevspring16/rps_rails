289dfcfeb0d3775cb1d859805921b5a13a4cf31d
Zachary C. Cornett <zcornett@chicagobooth.edu>
2016-04-22 18:23:39 -0500

    fail2
    I am not sure what happened to the last fail.  But, the webpage stopped
    working, so I restarted everything including the server.  So, now I have
    the overall code to functioning except sometimes I have true and false
    on some of the computer selections.  This is odd right?

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -9,31 +9,32 @@ class GameController < ApplicationController
 
     # Your logic here
 
-    # pc_options = ["rock", "paper","scissors"]
-    # @computer_move=pc_options.sample
-    @computer_move=["rock","paper","scissors"].sample
-
+    pc_options = ["rock", "paper","scissors"]
+    @computer_move=pc_options.sample
+    # @computer_move=["rock","paper","scissors"].sample
+    #
     if
-      # @computer_move == @user_move
-      # @result="Its a tie!"
-      
-      # elsif
+      @computer_move == @user_move
+      @result="tied!"
+
+      elsif
       @computer_move == "rock" && @user_move == "scissors"
-      @result= "I win"
-      # elsif @computer_move == "scissors" && @user_move =="paper"
-      #   @result= "I win"
-      # elsif @computer_move == "paper" && @user_move =="rock"
-      #   @result= "I win"
-      #
-      # elsif @computer_move =="rock" && @user_move =="paper"
-      #   @result= "You win"
-      # elsif @computer_move="scissors" && @user_move=="rock"
-      #   @result= "You win"
-      # elsif @computer_move=="paper" && @user_move=="scissors"
-      #   @result= "You win"
-    else
-      @result="test"
-    end
+      @result= "lose"
+      elsif @computer_move == "scissors" && @user_move =="paper"
+        @result= "lose"
+      elsif @computer_move == "paper" && @user_move =="rock"
+        @result= "lose"
+
+      elsif @computer_move =="rock" && @user_move =="paper"
+        @result= "win"
+      elsif @computer_move="scissors" && @user_move=="rock"
+        @result= "win"
+      elsif @computer_move=="paper" && @user_move=="scissors"
+        @result= "win"
+
+    # else
+    #   @result="test"
+   end
 
 
     # In the end, make sure you assign the correct values to the
