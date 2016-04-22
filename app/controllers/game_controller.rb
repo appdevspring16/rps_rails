8ab49067e51e21b5add977d23d0bd959565b565a
mchen7 <mchen7@chicagobooth.edu>
2016-04-22 03:02:46 -0500

    Final

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,36 +1,26 @@
 class GameController < ApplicationController
   def rps
 
-    @computer_move = ["Rock","Paper","Scissors"].sample
+    @user_move = params[:the_move]
 
-    @user_move = ["Rock","Paper","Scissors"].sample
+    @computer_move = ["rock","paper","scissors"].sample
 
-  #   if user_move.capitalize == moveset[0] or user_move.capitalize == moveset[1] or user_move.capitalize == moveset[2]
-  # puts "Valid Move! You Played " + user_move.capitalize
-
-  # cpu_move = moveset[rand(moveset.length)]
-  # puts "Computer Played " + cpu_move.capitalize
-
-  if @user_move == @computer_move
-    @result = "You Tied"
-  elsif @user_move == "Rock" && @computer_move == "Scissors"
-    @result = "You Won"
-  elsif @user_move == "Scissors" && @computer_move == "Paper"
-    @result = "You Won"
-  elsif @user_move == "Paper" && @computer_move == "Rock"
-    @result = "You Won"
-  elsif @user_move == "Rock" && @computer_move == "Paper"
-    @result = "You Lost"
-  elsif @user_move == "Paper" && @computer_move == "Scissors"
-    @result = "You Lost"
-  elsif @user_move == "Scissors" && @computer_move == "Rock"
-    @result = "You Lost"
-  end
-
-    # ===============================================================
-    # Your code goes above.
-    # ===============================================================
-
+    if @user_move == @computer_move
+      @result = "Tied"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @result = "Won"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @result = "Won"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @result = "Won"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @result = "Lost"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @result = "Lost"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @result = "Lost"
+    end
+    
     render("rps.html.erb")
   end
 end
