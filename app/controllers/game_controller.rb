c24731fb09cb86fff68a43c8d1731facff692ed8
mchen7 <mchen7@chicagobooth.edu>
2016-04-22 02:58:40 -0500

    Basic

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,20 +1,31 @@
 class GameController < ApplicationController
   def rps
-    @user_move = params[:the_move]
 
-    # ===============================================================
-    # Your code goes below.
-    # The move the user chose is in the variable @user_move.
-    # ===============================================================
+    @computer_move = ["Rock","Paper","Scissors"].sample
 
-    # Your logic here
+    @user_move = ["Rock","Paper","Scissors"].sample
 
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
+  #   if user_move.capitalize == moveset[0] or user_move.capitalize == moveset[1] or user_move.capitalize == moveset[2]
+  # puts "Valid Move! You Played " + user_move.capitalize
 
-    @computer_move = "Replace this string with the correct value."
+  # cpu_move = moveset[rand(moveset.length)]
+  # puts "Computer Played " + cpu_move.capitalize
 
-    @result = "Replace this string with the correct value."
+  if @user_move == @computer_move
+    @result = "You Tied"
+  elsif @user_move == "Rock" && @computer_move == "Scissors"
+    @result = "You Won"
+  elsif @user_move == "Scissors" && @computer_move == "Paper"
+    @result = "You Won"
+  elsif @user_move == "Paper" && @computer_move == "Rock"
+    @result = "You Won"
+  elsif @user_move == "Rock" && @computer_move == "Paper"
+    @result = "You Lost"
+  elsif @user_move == "Paper" && @computer_move == "Scissors"
+    @result = "You Lost"
+  elsif @user_move == "Scissors" && @computer_move == "Rock"
+    @result = "You Lost"
+  end
 
     # ===============================================================
     # Your code goes above.
