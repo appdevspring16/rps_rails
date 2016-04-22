c630a5bc38a767e9989c975f2f0ee4573d2a07ac
Andrea Ramirez <aramirez@users.noreply.github.com>
2016-04-22 14:46:54 -0500

    First try

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,13 +8,48 @@ class GameController < ApplicationController
     # ===============================================================
 
     # Your logic here
+    if @user_move == @computer_move
+      @computer_move = ["rock", "paper", "scissors"].sample
+      @result = ["You tied"]
 
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @computer_move = ["rock", "paper", "scissors"].sample
+
+      @result = ["You lose"]
+
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @computer_move = ["rock", "paper", "scissors"].sample
+
+      @result = ["You lose"]
+
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @computer_move = ["rock", "paper", "scissors"].sample
+
+      @result = ["You lose"]
+
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      p  @computer_move = ["rock", "paper", "scissors"].sample
+
+        @result = ["You win"]
+
+    elsif @user_move == "rock" && @computer_move == "paper"
+      p  @computer_move = ["rock", "paper", "scissors"].sample
+
+        @result = ["You lose"]
+
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @computer_move = ["rock", "paper", "scissors"].sample
+
+      @result = ["You win"]
+
+    end
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
 
-    @result = "Replace this string with the correct value."
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    @result = [result]
 
     # ===============================================================
     # Your code goes above.
