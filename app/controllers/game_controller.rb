0af6062124e1f0db5f08ba9ce07b795b82312b02
mmuntazir <mehdi.muntazir@outlook.com>
2016-04-22 23:05:16 -0500

    checkpoint 2

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,53 +1,30 @@
 class GameController < ApplicationController
   def rps
+    
     @user_move = params[:the_move]
 
-    # ===============================================================
-    # Your code goes below.
-    # The move the user chose is in the variable @user_move.
-    # ===============================================================
+    @computer_move = ["rock", "paper", "scissors"].sample
 
-    # Your logic here
-
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
-
-    @computer_move = ["rock", "paper", "scissors"m "quit"].sample
-
-    @result = "Replace this string with the correct value."
-
-    puts "Welcome to Rock, Paper, Scissors!"
-
-    puts "rock, paper, pr scissors?"
-
-    user_move = gets.chomp
-
-    puts "You chose #{user_move}"
-
-    choices = ["rock", "paper", "scissors"m "quit"]
-    if user_move="quit"
-      break
-    end
-
-    comp = choices[rand(0..2)]
-
-    new = user_move+comp
-    if new == "rockscissors"
-      puts "You win"
-      elseif new == "rockpaper"
-      puts"You lose"
-      elseif new == "paperscissors"
-      puts "You lose"
-      elseif new == "paperrock"
-      puts "You win"
-      elseif new == "scissorsrock"
-      puts "You lose"
-      elseif new == "scissorspaper"
-      puts "You lose"
+    if @user_move == @computer_move
+      xx = "It's a tie!"
+    elsif @user_move == "rock" and @computer_move == "paper"
+      xx = "You lose"
+    elsif @user_move == "rock" and @computer_move == "scissors"
+      xx = "You Win"
+    elsif @user_move == "paper" and @computer_move == "rock"
+      xx = "You Win"
+    elsif @user_move == "paper" and @computer_move == "scissors"
+      xx = "You lose"
+    elsif @user_move == "scissors" and @computer_move == "paper"
+      xx = "You Win"
+    elsif @user_move == "scissors" and @computer_move == "rock"
+      xx = "You lose"
     else
-      puts "Tied"
+      xx = "That doesn't seem to be a valid move."
     end
-    puts "Computer chose #{new}"
+
+
+    @result = xx
 
     # ===============================================================
     # Your code goes above.
