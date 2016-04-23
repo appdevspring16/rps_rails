c7ed5f7206f2a0c6f457e5a3e52b3b16d03e21f9
mustafa866 <mhassan@chicagobooth.edu>
2016-04-23 08:27:17 -0500

    initial logic

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,11 +8,32 @@ class GameController < ApplicationController
     # ===============================================================
 
     # Your logic here
+   
+    puts "You chose #{user_move}"
+    computer_move= ["rock", "paper", "scissors"].sample
+    puts "The computer chose #{computer_move}"
+
+    if @user_move == @computer_move #computer move = player move, result = tie
+        puts "You tied!" 
+    elsif @user_move == "rock" && @computer_move== "paper" # player move = rock & computer move = paper
+          puts "Computer wins!" # result = computer wins 
+    elsif @user_move == "rock" && @computer_move== "scissors" # player move = rock & computer move = scissors
+          puts "You win!" # result = player wins
+    elsif @user_move == "paper" && @computer_move== "rock"#player move = paper & computer move = rock
+          puts "Computer wins!" #result 
+    elsif @user_move == "paper" && @computer_move== "scissors" #player move = paper 
+          puts "Computer wins!" #result 
+    elsif @user_move == "scissors" && @computer_move== "rock" #player move = scissors
+          puts "Computer wins!" #result 
+    elsif @user_move == "scissors" && @computer_move== "paper" #player move = scissors
+          puts "You win!" #result      
+        
+    end
 
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = "rock."
 
     @result = "Replace this string with the correct value."
 
@@ -21,5 +42,5 @@ class GameController < ApplicationController
     # ===============================================================
 
     render("rps.html.erb")
-  end
 end
+  end
