5cc306490899bde4dec29b3117b0b93066312443
Matthew Keenan <mkeenan@chicagobooth.edu>
2016-04-23 09:28:04 -0500

    First pass

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,19 +2,36 @@ class GameController < ApplicationController
   def rps
     @user_move = params[:the_move]
 
-    # ===============================================================
-    # Your code goes below.
-    # The move the user chose is in the variable @user_move.
-    # ===============================================================
+    @user_score = 0
+    @comp_score = 0
+    
+    moves = ["rock", "paper", "scissors"]
 
-    # Your logic here
+    @computer_move = moves.sample
 
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
-
-    @computer_move = "Replace this string with the correct value."
-
-    @result = "Replace this string with the correct value."
+    if @user_move == @computer_move
+      @result = "It\'s a tie!"
+    elsif @user_move=="paper" && @computer_move =="rock"
+      @result = "You win!"
+      @user_score += 1
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @result = "You lose!"
+      @comp_score += 1
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @result = "You win!"
+      @user_score += 1
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @result =  "You lose!"
+      @comp_score += 1
+    elsif @user_move == "scissors" && @computer_move =="paper"
+      @result = "You win!"
+      @user_score += 1
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @result =  "You lose!"
+      @comp_score += 1
+    else
+      @result = "That doesn\'t appear to be a valid move."
+    end
 
     # ===============================================================
     # Your code goes above.
