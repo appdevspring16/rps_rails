a52b982bc18878ec19361e08c801b674cc31d119
vparikh0 <vparikh0@chicagobooth.edu>
2016-04-22 23:08:54 -0500

    Completed

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -12,9 +12,47 @@ class GameController < ApplicationController
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = ["rock", "paper", "scissor"].sample
+    choices = ["Rock", "Paper", "Scissors"]
 
-    @result = ["rock", "paper", "scissor"].sample
+    while choices.include?(@user_move.downcase.capitalize) == false
+      puts "That doesn\'t seem like a valid move."
+      puts "Pick again!"
+      break
+    end
+
+    @computer_move = choices.sample
+
+    puts  'Computer has picked ' + @computer_move
+
+    if @user_move.downcase.capitalize == @computer_move
+
+      @result = ["Its a Tie!"]
+      puts  ""
+      puts "Pick again!"
+
+    elsif @user_move.downcase.capitalize == "Rock" && @computer_move == "Scissors"
+      @result = ["You Win!"]
+      puts  ""
+      puts "Pick again!"
+
+    elsif @user_move.downcase.capitalize == "Scissors" && @computer_move == "Paper"
+      @result = ["You Win!"]
+      puts  ""
+      puts "Pick again!"
+
+    elsif @user_move.downcase.capitalize == "Paper" && @computer_move == "Rock"
+      @result = ["You Win!"]
+      puts  ""
+      puts "Pick again!"
+
+    else
+      @result = ["You Lose!"]
+
+      puts  ""
+      puts "Pick again!"
+
+    
+    end
 
     # ===============================================================
     # Your code goes above.
