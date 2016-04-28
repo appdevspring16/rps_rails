b0c97c0f8288ff0cc5c39ae893df0f6bbc62cb7b
Yelena Kulensova <Helen.Erikson@users.noreply.github.com>
2016-04-28 01:00:08 -0500

    Helen-Erikson-rps_rails-final submission
    completed rps_rails game

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,24 +1,42 @@
 class GameController < ApplicationController
+
   def rps
     @user_move = params[:the_move]
-
-    # ===============================================================
-    # Your code goes below.
     # The move the user chose is in the variable @user_move.
-    # ===============================================================
+    # In the end, make sure you assign the correct values to the following two variables:
 
-    # Your logic here
+    @computer_move = ["ROCK", "PAPER", "SCISSORS"].sample
+    # @computer_move is used instead of "puts computer_move"
+    # stuff with @ is called INSTANCE VARIABLE where we put some values, which is what the user will see in html
 
-    # In the end, make sure you assign the correct values to the
-    #   following two variables:
+    # if @user_move =="rock" && @user_move =="paper" && @user_move =="scissors"
+    # @result = "Replace this string with the correct value."
+    #won, lost, or tie
 
-    @computer_move = "Replace this string with the correct value."
+# now write bunch of logic that determines various outcomes
 
-    @result = "Replace this string with the correct value."
-
-    # ===============================================================
-    # Your code goes above.
-    # ===============================================================
+    if @user_move.upcase == @computer_move
+      @result = "tied :)"
+      #  @user_move = gets.chomp # game continues, user makes another move
+    elsif @user_move == "rock" && @computer_move == "PAPER"
+      @result = "lose"
+      # @user_move = gets.chomp # game continues, user makes another move
+    elsif @user_move == "rock" && @computer_move == "SCISSORS"
+      @result = "win!"
+      # @user_move = gets.chomp # game continues, user makes another move
+    elsif @user_move == "paper" && @computer_move == "ROCK"
+      @result = "win!"
+      # @user_move = gets.chomp # game continues, user makes another move
+    elsif @user_move == "paper" && @computer_move == "SCISSORS"
+      @result = "lose"
+      # @user_move = gets.chomp # game continues, user makes another move
+    elsif @user_move == "scissors" && @computer_move == "ROCK"
+      @result  = "lose"
+      # @user_move = gets.chomp # game continues, user makes another move
+    elsif @user_move == "scissors" && @computer_move == "PAPER"
+      @result = "win!"
+      # @user_move = gets.chomp # game continues, user makes another move
+    end
 
     render("rps.html.erb")
   end
